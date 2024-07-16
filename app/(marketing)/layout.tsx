f5408aa4c73d36518c4225d38396a4b6586460dc
TheGiantNinja <yasserfedsi34@gmail.com>
2024-07-16 16:05:47 +0200

    Done with marketing skeleton

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,6 @@
+import Footer from "./footer";
+import Header from "./header";
+
 type Props = {
   children: React.ReactNode;
 };
@@ -5,9 +8,11 @@ type Props = {
 const marketingLayout = ({ children }: Props) => {
   return (
     <div className="min-h-screen flex flex-col">
+      <Header />
       <main className="flex-1 flex flex-col items-center justify-center">
         {children}
       </main>
+      <Footer />
     </div>
   );
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,4 @@
 import { Button } from "@/components/ui/button";
 export default function Home() {
-  return (
-    <div className="text-center justify-center p-5">
-      <p className="text-red-600 text-3xl font-extrabold">
-        Welcome To Duolingo!!
-      </p>
-      <Button className="text-black">Click Me!!</Button>
-    </div>
-  );
+  return <div> This is a Marketing Page</div>;
 }
