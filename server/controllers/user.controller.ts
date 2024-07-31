012987923a43e5cad6a058a75bf3d52f364bbc8b
yasserfds <yasserfedsi34@gmail.com>
2024-07-31 20:47:40 +0200

    Done with User Activation

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -84,3 +84,43 @@ export const createActivationToken = (user: any): IActivationToken => {
 
   return { token, activationCode };
 };
+
+// Activate User
+interface IActivationRequest {
+  activation_token: string;
+  activation_code: string;
+}
+
+export const activateUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } =
+        req.body as IActivationRequest;
+
+      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET as string
+      ) as { user: IUser; activationCode: string };
+
+      if (newUser.activationCode !== activation_code) {
+        return next(new errorHandler("Invalid activation code", 400));
+      }
+
+      const { name, email, password } = newUser.user;
+
+      const existUser = await userModel.findOne({ email });
+
+      if (existUser) {
+        return next(new errorHandler("Email already exist", 400));
+      }
+
+      const user = await userModel.create({ name, email, password });
+
+      res.status(201).json({
+        success: true,
+      });
+    } catch (error: any) {
+      return next(new errorHandler(error.message, 400));
+    }
+  }
+);
