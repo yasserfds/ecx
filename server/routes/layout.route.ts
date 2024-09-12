5ad91f957fcde2711c280f054f643541311f2d20
Yasser <yasserfedsi34@gmail.com>
2024-09-12 13:35:32 +0100

    Add comments

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,27 +1,29 @@
-import express from "express";
-import { authorizeRoles, isAuthenticated } from "../middleware/auth";
+// Importing necessary modules and middleware
+import express from "express"; // Express framework for building web applications
+import { authorizeRoles, isAuthenticated } from "../middleware/auth"; // Middleware for authentication and role-based authorization
 import {
-  createLayout,
-  editLayout,
-  getLayoutByType,
+  createLayout, // Controller function to create a new layout
+  editLayout, // Controller function to edit an existing layout
+  getLayoutByType, // Controller function to get layout by type
 } from "../controllers/layout.controller";
 
-const layoutRouter = express.Router();
+const layoutRouter = express.Router(); // Create a new router instance for layout-related routes
 
+// Route to create a new layout
+// This route is protected by authentication and authorization middleware
+// Only authenticated users with the 'admin' role can access this route
 layoutRouter.post(
   "/create-layout",
-  isAuthenticated,
-  authorizeRoles("admin"),
-  createLayout
+  isAuthenticated, // Middleware to check if the user is authenticated
+  authorizeRoles("admin"), // Middleware to authorize access for users with the 'admin' role
+  createLayout // Controller function to handle the creation of a layout
 );
 
+// Route to edit an existing layout
+// This route is also protected by authentication and authorization middleware
+// Only authenticated users with the 'admin' role can access this route
 layoutRouter.put(
   "/edit-layout",
-  isAuthenticated,
-  authorizeRoles("admin"),
-  editLayout
+  isAuthenticated, // Middleware to check if the user is authenticated
+  authorizeRoles("admin") // Middleware to authorize access for users with the 'admin' role
 );
-
-layoutRouter.get("/get-layout", isAuthenticated, getLayoutByType);
-
-export default layoutRouter;
